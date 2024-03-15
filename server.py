79ca0d8f9d8c7c090c439475a7a7c5b3d8fc748b
Cindy Wong <cindywongdev@gmail.com>
2024-03-15 15:41:02 +0100

    successfully connected to randomuser API

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
 from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
+response = requests.get("https://randomuser.me/api")
+print(f'hi {response.status_code}')
+
 @app.route("/", methods=["GET"])
 def HOME_ROUTE():
-    return {"response": [1,2,3,4,5]}
+    return response.json()
+    # return {"response": [1,2,3,4,5]}
 
 
 @app.route("/params/<one>/<two>", methods=["GET", "POST"])
@@ -16,5 +21,5 @@ def PARAMS_ROUTE(one, two):
             "query": request.args.get("cheese")
         }
     if (request.method) == "POST":
-        body = request.json
+        body = request.json #do i need () here?
         return body
