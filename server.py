72ca20566dc47219ac02bfb1284fbc308ab9dfd6
Cindy Wong <cindywongdev@gmail.com>
2024-03-20 15:00:25 -0400

    filtered results for payments over

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ CORS(app)
 # ------------------------------------------------------------------------------------
 
 # but for now, just use static endpoint for 2022 general payments
-response = requests.get("https://openpaymentsdata.cms.gov/api/1/datastore/query/66dfcf9a-2a9e-54b7-a0fe-cae3e42f3e8f?limit=20")
+response = requests.get("https://openpaymentsdata.cms.gov/api/1/datastore/query/66dfcf9a-2a9e-54b7-a0fe-cae3e42f3e8f")
 
 @app.route("/", methods=["GET"])
 def HOME_ROUTE():
@@ -18,16 +18,26 @@ def HOME_ROUTE():
     
 @app.route("/data", methods=["GET"])
 def DATA_ROUTE():
-    return response.json()
+    all_payments = response.json()["results"]
+    print(len(all_payments))
     
-@app.route("/params/<one>/<two>", methods=["GET", "POST"])
-def PARAMS_ROUTE(one, two):
-    if (request.method) == "GET":
-        return {
-            "one": one,
-            "two": two,
-            "query": request.args.get("cheese")
-        }
-    if (request.method) == "POST":
-        body = request.json #do i need () here for request?
-        return body
+    # The following code returns all payments > $10. This is because all the fields in database are string types. Although the API offers using SQL in the endpoints as an option to return a specific query, it doesn't work when interacting with non-string types, such as floats, in this case. Therefore it must be manually done in this backend.
+    payments_over_10 = []
+    for payment in all_payments: 
+        payment_int = float(payment["total_amount_of_payment_usdollars"])
+        payments_over_10.append(payment) if payment_int >= 10 else None
+    
+    print(len(payments_over_10))
+    return payments_over_10
+    
+# @app.route("/params/<one>/<two>", methods=["GET", "POST"])
+# def PARAMS_ROUTE(one, two):
+#     if (request.method) == "GET":
+#         return {
+#             "one": one,
+#             "two": two,
+#             "query": request.args.get("cheese")
+#         }
+#     if (request.method) == "POST":
+#         body = request.json #do i need () here for request?
+#         return body
